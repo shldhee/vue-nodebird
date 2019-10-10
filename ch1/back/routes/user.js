d6f73f0472055a7d5ba53a5d68b6c4532e3dbf94
Duckhee Lee <shldhee@yahoo.co.kr>
2019-10-11 00:41:40 +0900

    copy and paste

--- ch1/back/routes/user.js
+++ ch1/back/routes/user.js
@@ -1,41 +1,39 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const db = require("../models");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const db = require('../models');
 const {
   isLoggedIn,
   isNotLoggedIn
-} = require("./middlewares");
+} = require('./middlewares');
+
 const router = express.Router();
 
-router.get("/", isLoggedIn, async (req, res, next) => {
+router.get('/', isLoggedIn, async (req, res, next) => {
   const user = req.user;
   res.json(user);
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const user = await db.User.findOne({
       where: {
         id: parseInt(req.params.id, 10)
       },
       include: [{
-          model: db.Post,
-          as: "Posts",
-          attributes: ["id"]
-        },
-        {
-          model: db.User,
-          as: "Followings",
-          attributes: ["id"]
-        },
-        {
-          model: db.User,
-          as: "Followers",
-          attributes: ["id"]
-        }
-      ],
-      attributes: ["id", "nickname"]
+        model: db.Post,
+        as: 'Posts',
+        attributes: ['id'],
+      }, {
+        model: db.User,
+        as: 'Followings',
+        attributes: ['id'],
+      }, {
+        model: db.User,
+        as: 'Followers',
+        attributes: ['id'],
+      }],
+      attributes: ['id', 'nickname'],
     });
     res.json(user);
   } catch (err) {
@@ -103,81 +101,56 @@ router.post('/', isNotLoggedIn, async (req, res, next) => { // 회원가입
   }
 });
 
-router.post('/', isNotLoggedIn, async (req, res, next) => { // 회원가입
-  try {
-    const hash = await bcrypt.hash(req.body.password, 12);
-    const exUser = await db.User.findOne({
-      where: {
-        userId: req.body.userId,
-      },
-    });
-    if (exUser) { // 이미 회원가입되어있으면
-      return res.status(403).json({
-        errorCode: 1,
-        message: '이미 회원가입되어있습니다.',
-      });
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
     }
-    await db.User.create({
-      userId: req.body.userId,
-      password: hash,
-      nickname: req.body.nickname,
-    }); // HTTP STATUS CODE
-    passport.authenticate('local', (err, user, info) => {
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async (err) => { // 세션에다 사용자 정보 저장 (어떻게? serializeUser)
       if (err) {
         console.error(err);
         return next(err);
       }
-      if (info) {
-        return res.status(401).send(info.reason);
-      }
-      return req.login(user, async (err) => { // 세션에다 사용자 정보 저장 (어떻게? serializeUser)
-        if (err) {
-          console.error(err);
-          return next(err);
-        }
-        const fullUser = await db.User.findOne({
-          where: {
-            id: user.id
-          },
-          attributes: ['id', 'userId', 'nickname'],
-          include: [{
-            model: db.Post,
-            attributes: ['id'],
-          }, {
-            model: db.User,
-            as: 'Followings',
-            attributes: ['id'],
-          }, {
-            model: db.User,
-            as: 'Followers',
-            attributes: ['id'],
-          }],
-        });
-        return res.json(fullUser);
+      const fullUser = await db.User.findOne({
+        where: {
+          id: user.id
+        },
+        attributes: ['id', 'userId', 'nickname'],
+        include: [{
+          model: db.Post,
+          attributes: ['id'],
+        }, {
+          model: db.User,
+          as: 'Followings',
+          attributes: ['id'],
+        }, {
+          model: db.User,
+          as: 'Followers',
+          attributes: ['id'],
+        }],
       });
-    })(req, res, next);
-  } catch (err) {
-    console.log(err);
-    return next(err);
-  }
+      return res.json(fullUser);
+    });
+  })(req, res, next);
 });
 
-
-
-
-router.post("/logout", isLoggedIn, (req, res, next) => {
-  if (req.isAuthenticated) {
-    req.logout(); // 필수
+router.post('/logout', isLoggedIn, (req, res) => { // 실제 주소는 /user/logout
+  if (req.isAuthenticated()) {
+    req.logout();
     req.session.destroy(); // 선택사항
-    return res.status(200).send("로그아웃이 되었습니다.");
+    return res.status(200).send('로그아웃 되었습니다.');
   }
 });
 
-router.get("/:id/posts", async (req, res, next) => {
+router.get('/:id/posts', async (req, res, next) => {
   try {
     let where = {
       UserId: parseInt(req.params.id, 10),
-      RetweetId: null
+      RetweetId: null,
     };
     if (parseInt(req.query.lastId, 10)) {
       where[db.Sequelize.Op.lt] = parseInt(req.query.lastId, 10);
@@ -185,19 +158,16 @@ router.get("/:id/posts", async (req, res, next) => {
     const posts = await db.Post.findAll({
       where,
       include: [{
-          model: db.User,
-          attributes: ["id", "nickname"]
-        },
-        {
-          model: db.Image
-        },
-        {
-          model: db.User,
-          through: "Like",
-          as: "Likers",
-          attributes: ["id"]
-        }
-      ]
+        model: db.User,
+        attributes: ['id', 'nickname'],
+      }, {
+        model: db.Image,
+      }, {
+        model: db.User,
+        through: 'Like',
+        as: 'Likers',
+        attributes: ['id'],
+      }],
     });
     res.json(posts);
   } catch (e) {
@@ -206,27 +176,27 @@ router.get("/:id/posts", async (req, res, next) => {
   }
 });
 
-router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
+router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   try {
     const me = await db.User.findOne({
       where: {
         id: req.user.id
-      }
+      },
     });
     await me.addFollowing(req.params.id);
     res.send(req.params.id);
-  } catch (error) {
-    console.err(error);
-    next(error);
+  } catch (e) {
+    console.error(e);
+    next(e);
   }
 });
 
-router.delete("/:id/follow", isLoggedIn, async (req, res, next) => {
+router.delete('/:id/follow', isLoggedIn, async (req, res, next) => {
   try {
     const me = await db.User.findOne({
       where: {
         id: req.user.id
-      }
+      },
     });
     await me.removeFollowing(req.params.id);
     res.send(req.params.id);
@@ -236,14 +206,14 @@ router.delete("/:id/follow", isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.patch("/nickname", isLoggedIn, async (req, res, next) => {
+router.patch('/nickname', isLoggedIn, async (req, res, next) => {
   try {
     await db.User.update({
-      nickname: req.body.nickname
+      nickname: req.body.nickname,
     }, {
       where: {
         id: req.user.id
-      }
+      },
     });
     res.send(req.body.nickname);
   } catch (e) {
@@ -252,17 +222,17 @@ router.patch("/nickname", isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.get("/:id/followings", isLoggedIn, async (req, res, next) => {
+router.get('/:id/followings', isLoggedIn, async (req, res, next) => {
   try {
     const user = await db.User.findOne({
       where: {
         id: req.user.id
-      }
+      },
     });
     const followings = await user.getFollowings({
-      attributes: ["id", "nickname"],
+      attributes: ['id', 'nickname'],
       limit: parseInt(req.query.limit || 3, 10),
-      offset: parseInt(req.query.offset || 0, 10)
+      offset: parseInt(req.query.offset || 0, 10),
     });
     res.json(followings);
   } catch (err) {
@@ -271,17 +241,17 @@ router.get("/:id/followings", isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.get("/:id/followers", isLoggedIn, async (req, res, next) => {
+router.get('/:id/followers', isLoggedIn, async (req, res, next) => {
   try {
     const user = await db.User.findOne({
       where: {
         id: req.user.id
-      }
+      },
     });
     const followers = await user.getFollowers({
-      attributes: ["id", "nickname"],
+      attributes: ['id', 'nickname'],
       limit: parseInt(req.query.limit || 3, 10),
-      offset: parseInt(req.query.offset || 0, 10)
+      offset: parseInt(req.query.offset || 0, 10),
     });
     res.json(followers);
   } catch (err) {
@@ -290,12 +260,12 @@ router.get("/:id/followers", isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.delete("/:id/follower", isLoggedIn, async (req, res, next) => {
+router.delete('/:id/follower', isLoggedIn, async (req, res, next) => {
   try {
     const me = await db.User.findOne({
       where: {
         id: req.user.id
-      }
+      },
     });
     await me.removeFollower(req.params.id);
     res.send(req.params.id);
@@ -304,4 +274,5 @@ router.delete("/:id/follower", isLoggedIn, async (req, res, next) => {
     next(err);
   }
 });
+
 module.exports = router;
