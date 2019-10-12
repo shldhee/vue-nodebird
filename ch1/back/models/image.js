7636bd878af289c4e8e818dfa4394f10c77aa3cb
Duckhee Lee <shldhee@yahoo.co.kr>
2019-10-12 12:46:14 +0900

    update image src data length

--- ch1/back/models/image.js
+++ ch1/back/models/image.js
@@ -1,14 +1,12 @@
 // 이미지 저장용 서버에 저장하고 그 주소를 디비에다 저장
 module.exports = (sequelize, DataTypes) => {
   const Image = sequelize.define(
-    "Image",
-    {
+    "Image", {
       src: {
-        type: DataTypes.STRING(200),
+        type: DataTypes.STRING(2000),
         allowNull: false
       }
-    },
-    {
+    }, {
       charset: "utf8",
       collate: "utf8_general_ci" // 한글저장됩니다
     }
